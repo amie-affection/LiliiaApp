d404ee6e81285b5944a596faf397783fd6e57206
amie-affection <amie.affection@gmail.com>
2021-03-25 17:41:31 +0200

    added modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import './burger.js';
+import "./burger.js";
 
 // script for unzoom on the page-card
 
@@ -17,20 +17,20 @@ document.addEventListener(
 
 //
 
-window.scroll({ behavior: 'smooth' });
+window.scroll({ behavior: "smooth" });
 
-document.addEventListener('DOMContentLoaded', function () {
-  let node = document.querySelector('.preload-transitions');
-  node.classList.remove('preload-transitions');
+document.addEventListener("DOMContentLoaded", function () {
+  let node = document.querySelector(".preload-transitions");
+  node.classList.remove("preload-transitions");
 });
 
 // bottom-menu parser from sections
 function createBottomMenu() {
-  const root = document.querySelector('.bottom__menu-inner');
-  const products = document.querySelectorAll('.products');
+  const root = document.querySelector(".bottom__menu-inner");
+  const products = document.querySelectorAll(".products");
 
   products.forEach((el) => {
-    const title = el.querySelector('.products__title-text').innerText;
+    const title = el.querySelector(".products__title-text").innerText;
     const item = `
         <li class="bottom__menu-item" data-tap='${el.id}'>
           <a class="bottom__menu-link" href="#${el.id}">${title}</a>
@@ -38,23 +38,23 @@ function createBottomMenu() {
     `;
     root.innerHTML += item;
   });
-  const item = document.querySelector('.bottom__menu-item');
-  item.classList.add('active');
+  const item = document.querySelector(".bottom__menu-item");
+  item.classList.add("active");
 }
 createBottomMenu();
 
 // scroll methods
 const scroll = {
-  bars: document.querySelectorAll('.bottom__menu-item'),
-  products: document.querySelectorAll('.products'),
+  bars: document.querySelectorAll(".bottom__menu-item"),
+  products: document.querySelectorAll(".products"),
 
   remove: function () {
-    this.bars.forEach((el) => el.classList.remove('active'));
+    this.bars.forEach((el) => el.classList.remove("active"));
   },
 
   chose: function (num) {
     this.remove();
-    this.bars[num].classList.add('active');
+    this.bars[num].classList.add("active");
   },
 
   scrollBar: function () {
@@ -71,63 +71,143 @@ const scroll = {
     });
   },
 };
-document.addEventListener('scroll', scroll.scrollBar);
+document.addEventListener("scroll", scroll.scrollBar);
 
 // tap scroll to
 (function scrollTo() {
-  const tapBar = document.querySelectorAll('.bottom__menu-item');
-  const products = document.querySelectorAll('.products');
+  const tapBar = document.querySelectorAll(".bottom__menu-item");
+  const products = document.querySelectorAll(".products");
 
   const goToHeader = (event) => {
     event.preventDefault();
-    const element = event.target.getAttribute('data-tap');
+    const element = event.target.getAttribute("data-tap");
 
     products.forEach((el) => {
       if (el.id == element) {
         const topElCord = el.getBoundingClientRect().top + pageYOffset;
-        window.scrollTo({ top: topElCord - 140, left: 0, behavior: 'smooth' });
+        window.scrollTo({ top: topElCord - 140, left: 0, behavior: "smooth" });
       }
     });
   };
 
-  tapBar.forEach((el) => el.addEventListener('click', goToHeader));
+  tapBar.forEach((el) => el.addEventListener("click", goToHeader));
 })();
+
+// modal window
+
+function showModal() {
+  const sliderItem = document.querySelectorAll(".slider__item");
+  const modal = document.querySelector(".modal__window");
+  const modalBuffer = document.querySelector(".modal__buffer");
+  const header = document.querySelector(".header");
+  const slider = document.querySelector(".slider");
+  const products = document.querySelector(".products");
+  const sliderInner = document.querySelector(".slider__inner");
+
+  const modalState = (action) => {
+    modal.classList[action]("show");
+    modalBuffer.classList[action]("show");
+    header.classList.add("unfixed");
+    header.classList.add("blur");
+    slider.classList.add("blur");
+    products.classList.add("blur");
+    sliderInner.classList.add("show");
+    document.body.classList[action]("scroll-off");
+  };
+
+  const showCard = (object) => {
+    const modalItem = document.querySelector(".modal__window");
+    const name = document.querySelector(".slider__description").innerText;
+
+    console.log(name);
+
+    const card = {
+      img: object.src,
+      // name: object.querySelector(".slider__description"),
+      // description: obj.querySelector(".product__item-description").innerText,
+    };
+
+    const modalLayout = `
+      <div class="modal__window-img">
+        <img
+          src="${card.img}"
+          alt=""
+          onerror="this.style.display='none'" loading="lazy"
+          height="162" width="240"
+        />
+      </div>
+
+      <div>
+        <h3 class="modal__title">${name}</h3>
+        <p class="modal__description">${card.description}</p>
+      </div>
+    `;
+
+    modalItem.innerHTML = modalLayout;
+  };
+
+  const openModal = (event) => {
+    showCard(event.target);
+    modalState("add");
+
+    modalBuffer.addEventListener("click", () => {
+      modalState("remove");
+      header.classList.remove("unfixed");
+      header.classList.remove("blur");
+      slider.classList.remove("blur");
+      products.classList.remove("blur");
+      sliderInner.classList.remove("show");
+    });
+  };
+
+  sliderItem.forEach((el) => el.addEventListener("click", openModal));
+}
+showModal();
 
 // product pop-up
 function productPopUp() {
-  const products = document.querySelectorAll('.product__item');
-  const popup = document.querySelector('.popup');
-  const close = document.querySelector('.popup__close');
-  const buffer = document.querySelector('.popup__buffer');
+  const products = document.querySelectorAll(".product__item");
+  const popup = document.querySelector(".popup");
+  const close = document.querySelector(".popup__close");
+  const buffer = document.querySelector(".popup__buffer");
 
   const popUpState = (action) => {
-    popup.classList[action]('open');
-    buffer.classList[action]('open');
-    document.body.classList[action]('scroll-off');
+    popup.classList[action]("open");
+    buffer.classList[action]("open");
+    document.body.classList[action]("scroll-off");
   };
 
   const creatProductCard = (obj) => {
-    const popUpItem = document.querySelector('.popup__product');
+    const popUpItem = document.querySelector(".popup__product");
+
+    console.log(obj)
 
     const card = {
-      img: obj.querySelector('.product__item-img>img').src,
-      name: obj.querySelector('.product__item-title>span').innerText,
-      price: obj.querySelector('.product__item-price').innerText,
-      description: obj.querySelector('.product__item-description').innerText,
+      img: obj.querySelector(".product__item-img>img").src,
+      name: obj.querySelector(".product__item-title>span").innerText,
+      price: obj.querySelector(".product__item-price").innerText,
+      description: obj.querySelector(".product__item-description").innerText,
     };
 
     const popUpLayout = `
          <div class="popup__item-img">
-          <img src="${card.img}" onerror="this.style.display='none'" alt="">
+            <img src="${card.img}" onerror="this.style.display='none'" alt="">
           </div>
-          <h3 class="popup__item-name">${card.name}</h3>
+
+          <div class="popup__title-price">
+            <h3 class="popup__item-name">${card.name}</h3>
+            <p class="popup__item-price">${card.price}</p>        
+          </div>
+
+          <div class="pop">
             <div class="popup__item-text">
               <h3 class="popup__item-title">Menu du jour</h3>
               <p class="popup__item-description">
               <span>${card.description}</span>
               </p>
-              </div>
-                        <div class="popup__adds">
+            </div>
+            <div class="popup__adds">
+          </div>
 
 
             <div class="popup__add popup__add-ingredient">
@@ -157,8 +237,6 @@ function productPopUp() {
               </ul>
             </div>
           </div>
-           <p class="popup__item-price">${card.price}</p>
-
     `;
 
     popUpItem.innerHTML = popUpLayout;
@@ -166,16 +244,16 @@ function productPopUp() {
 
   const popUp = (event) => {
     creatProductCard(event.target);
-    popUpState('add');
+    popUpState("add");
 
-    close.addEventListener('click', () => {
-      popUpState('remove');
+    close.addEventListener("click", () => {
+      popUpState("remove");
     });
-    buffer.addEventListener('click', () => {
-      popUpState('remove');
+    buffer.addEventListener("click", () => {
+      popUpState("remove");
     });
   };
 
-  products.forEach((el) => el.addEventListener('click', popUp));
+  products.forEach((el) => el.addEventListener("click", popUp));
 }
 productPopUp();
